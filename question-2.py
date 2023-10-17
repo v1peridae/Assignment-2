f105f77f215902e91ce49b1170a7b74377aa393b
louisa <82815171+v1peridae@users.noreply.github.com>
2023-10-17 03:33:58 -0700

    Create question-2.py
    https://replit.com/@viperidae/Assignment-22#main.py

---
+++ question-2.py
@@ -0,0 +1,6 @@
+item = input('Please input the price of your item >')
+item = float(item)
+amount = input ('Please input the total amount of money you have given >')
+amount = float(amount)
+change = amount - item
+print ('Your change will be $',change)
